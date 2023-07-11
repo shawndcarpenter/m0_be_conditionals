f3097cdcf919b70ac647c2b675b84e87f594f544
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-10 21:49:24 -0600

    Finish spicy.rb

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -5,3 +5,16 @@
 # If the Integer is a multiple of both 3 and 5, print "FizzBuzz". 
 # If the Integer is not a multiple of either, print the Integer itself.
 
+number = 26
+if
+    number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+elsif
+    number % 3 == 0
+    puts "Fizz"
+elsif
+    number % 5 == 0
+    puts "Buzz"
+else
+    puts number
+end
