e09d7bbb1251509fe13a5b5b64e0c5a13b7523f2
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-10 21:03:40 -0600

    Finish answering questions in comparisons.rb

--- comparisons.rb
+++ comparisons.rb
@@ -62,20 +62,26 @@ number_students <= 21
 
 puts 4 < 9
 #YOU DO: Explain.
-
+# This line of code will print out the result of the comparison of 4 < 9, which asks the question, is an integer of 4 less than an integer of 9?
+# The comparison will evaluate to true, as 4 is less than 9.
 books = 3
 puts 4 < books
 # YOU DO: Explain.
-
+# This code assigns the variable "books" , which holds a value which is an integer of 3. Then, the code will print the result of the comparison of "4 < books" , which asks the question, is an integer of 4 less than the value (3) stored within the variable "books" ? 
+# The comparison will evaluate to false, as 4 is NOT less than 3.
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-
+# This code assigns the variable "friends," which holds a value of 6. Then, this code assigns a variable of "siblings" which holds a value of 2. The code then prints the result of the comparison "friends > siblings," which asks "is the value stored in 'friends' greater than the value stored in 'siblings'" ?
+# This comparison will evaluate to true, as the value stored in friends is greater than the value stored in siblings.
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+# This code assigns the variable "attendees" which holds a value of 9. The code then assigns the variable "meals" which holds a value of 8. 
+# The code then prints the result of the comparison "attendees != meals", which asks the question "is the value stored in attendees not equal to the valur stored in meals?"
+# This comparison will evaluate to true, as the value stored in attendees is not equal to the value stored in meals.
 
 
 #-------------------
@@ -98,15 +104,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && age < 1
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: 
+# My final line of code evaluated to false, because I defined a dog that is a puppy as a dog that is less than 1 year old. 
+# even though loves_to_play = true, because we are using the logical operator of && which requires both conditions to be met, the output was false.
